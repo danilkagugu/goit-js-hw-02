e465d221f7987f44cbf04172dcf2cfa6800311aa
danilkagugu <danilyanishevski2001@gmail.com>
2023-10-31 13:16:36 +0200

    homework-2

---
+++ js/task-4.js
@@ -0,0 +1,28 @@
+function getShippingCost(country) {
+  let howMuch = "";
+  switch (country) {
+    case "China":
+      howMuch = 100;
+      break;
+    case "Chile":
+      howMuch = 250;
+      break;
+    case "Australia":
+      howMuch = 170;
+      break;
+    case "Jamaica":
+      howMuch = 120;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+
+  return `Shipping to ${country} will cost ${howMuch} credits`;
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
